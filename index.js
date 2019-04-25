620bdfde649074cfb3470174974ecc0bd130e757
memizzos <44787449+memizzos@users.noreply.github.com>
2019-04-25 20:01:48 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,33 +1,44 @@
 const api = 'https://pokeapi.co/api/v2'
 
-function buildPokemon(name){
-    return `
-    <div class="row">
-    <div class="col">
-    <span>${name}</span>
-    <img src="">
-    <span>typ</span>
-    <span>waga</span>
-    </div>
-    </div>
-    `
+function buildPokemon(pokemon) {
+	return `
+		<div class = "row pokemon">
+			<img src="">
+			<div class="col">
+				<span>${pokemon.name}</span>
+				<span>type</span>
+				<span>${pokemon.weight}</span>
+			</div>
+		</div>
+	`
 }
 
-function loadPokemons(){
-    fetch(api + "/pokemon")
-    .then(function (resp){
-        return resp.json();
-    })
-    .then(function (json){
-        console.log(json)
-        const listaPokemonow = json.results;
-        const listaHtmliPokemonow = listaPokemonow.map(
-        function(pokemon){return buildPokemon(pokemon.name);})
-        const htmlWszystkichPokemonow = listaHtmliPokemonow.join();
-        document.querySelector("#pokemony").innerHTML=htmlWszystkichPokemonow;
+function loadPokemons() {
+	fetch(api + "/pokemon")
+		.then(function (resp) {
+			return resp.json();
+		})
+		.then(function (json) {
+			let listaPokemonow = json.results;
+			let requesty = listaPokemonow.map(
+				(pokemon) => {
+					return fetch(pokemon.url)
+						.then(function (resp) {
+							return resp.json();
+						})
+				})
+			Promise.all(requesty).then(function (pokemony) {
+				const listaPokemonow = json.results; // [{},{}]
+				const listaHtmliPokemonow = pokemony.map((pokemon) => buildPokemon(pokemon))
+				const htmlWszystkichPokemonow = listaHtmliPokemonow.join(""); // ""
+				document.querySelector("#pokemony").innerHTML = htmlWszystkichPokemonow;
 
-    })
-    .catch(function (err){
-        console.error(err)
-    })
-}
+			})
+		})
+		.catch(function (err) {
+			console.error(err)
+		})
+	}
+	document.addEventListener("load", loadPokemons)
+	loadPokemons()
+	
